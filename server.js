a4ea89d4ecd60d89b8339368dbb05afc5858962f
Godfred Nai <godfrednai@gmail.com>
2024-03-19 22:53:09 +0000

    middleware for routes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
+const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/products");
+const transactionRoutes = require("./routes/transactions");
+const categoryRoutes = require("./routes/categories");
+
 const app = express();
 
 app.use(express.json());
@@ -16,9 +21,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/", (req, res) => {
-  res.send("Welcome to inventory");
-});
+app.use("/api/user", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
