48431e2b1fd9d7bcde64112509a211ab2e4c9086
Godfred Nai <godfrednai@gmail.com>
2024-03-27 03:09:41 +0000

    imported controller functions for various routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,42 +1,22 @@
 const express = require("express");
+const {
+  addCategory,
+  getCategories,
+  getOneCategory,
+  deleteCategory,
+  updateCategory,
+} = require("../controllers/categoryController");
 
 const router = express.Router();
 
+router.get("/", getCategories);
 
-router.get("/", (req, res) => {
-  res.json({
-    products: [
-      { id: 1, name: "food" },
-      { id: 2, name: "drinks" },
-      { id: 3, name: "electronics" },
-      { id: 4, name: "tools" },
-    ],
-  });
-});
+router.get("/:id", getOneCategory);
 
-router.get("/:id", (req, res) => {
-  res.json({
-    message: "Get single category",
-  });
-});
+router.post("/", addCategory);
 
-router.post("/", (req, res) => {
-  res.json({
-    message: "create a new category",
-  });
-});
+router.delete("/:id", deleteCategory);
 
-router.delete("/:id", (req, res) => {
-  res.json({
-    message: "delete a category",
-  });
-});
-
-router.patch("/:id", (req, res) => {
-  res.json({
-    message: "update a category",
-  });
-});
-
+router.patch("/:id", updateCategory);
 
 module.exports = router;
