b039c243ecbd9ec16083dc76c95d760095d24660
Godfred Nai <godfrednai@gmail.com>
2024-03-28 08:55:58 +0000

    minor changes

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const colors = require("colors");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
+const errorHandler = require("./middleware/errorHandler");
 
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/products");
 const transactionRoutes = require("./routes/transactions");
 const categoryRoutes = require("./routes/categories");
+const quantityUnitRoutes = require("./routes/quantityUnits");
+const productQuantityUnitRoutes = require("./routes/productQuantityUnits");
 
 const app = express();
 
@@ -17,7 +20,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method.red);
+  console.log(req.path, req.method.yellow);
   next();
 });
 
@@ -25,6 +28,10 @@ app.use("/api/user", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/transactions", transactionRoutes);
+app.use("/api/quantityunits", quantityUnitRoutes);
+app.use("/api/productquantityunits", productQuantityUnitRoutes);
+
+app.use(errorHandler);
 
 mongoose
   .connect(process.env.MONGO_URI)
